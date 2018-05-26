1ec2fb3f97e8f0539f3af2b99a8e17b4f58208ce
feiys <feiyuanshan@qq.com>
2018-05-26 11:41:06 +0800

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <blog-header></blog-header>
+    <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+import AddBlog from './components/AddBlog'
+import ShowBlogs from './components/ShowBlogs'
+import BlogHeader from './components/BlogHeader'
+
+export default {
+  name: 'app',
+  components: {
+    AddBlog,ShowBlogs,BlogHeader
+  }
+}
+</script>
+
+<style>
+ul{
+  list-style-type: none;
+  text-align: center;
+  margin: 0;
+}
+li{
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/" exact>博客</router-link>
+        <router-link to="/add" exact>写博客</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name:'blog-header',
+  // components:{
+  //   AddBlog,ShowBlogs,BlogHeader
+  //}
+}
+</script>
+
+<style scope>
+ul{
+  list-style-type: none;
+  text-align: left;
+  margin: 0;
+}
+li{
+  display: inline-block;
+  margin: 0 10px;
+}
+a{
+color:blue;
+text-decoration: none;
+padding: 12px;
+border-radius: 5px;
+}
+nav{
+  background: crimson;
+  padding: 30px 0;
+  margin-bottom: 40px;
+}
+.router-link-active{
+  background: rgba(255,255,255,0.8);
+  color: #444;
+}
+</style>
